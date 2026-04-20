c6bc6daee79bd981b7b27e7d18a7a46228bf044d
Tanel Marran <tanel@play.ee>
2026-04-20 21:56:39 +0200

    [main] adds a few poses

--- scenes/objects/parkourguy/gd_parkour_guy.gd
+++ scenes/objects/parkourguy/gd_parkour_guy.gd
@@ -1,13 +1,28 @@
 class_name ParkourGuy extends Node3D
 
+@export var animation_player: AnimationPlayer
+
+var _action_map: Dictionary = {
+	'key_up': 'up',
+	'key_down': 'down'
+}
+
 func _input(event):
 	# Mouse in viewport coordinates.
-	if event is InputEventMouseButton:
-		print("Mouse Click/Unclick at: ", event.position)
-	elif event is InputEventMouseMotion:
-		
+	if event is InputEventMouseMotion:
 		follow_mouse(event)
-
+	
+func _process(delta):
+	var action_mapped: bool = false
+	for action in _action_map.keys():
+		if Input.is_action_pressed(action):
+			animation_player.play(_action_map.get(action))
+			action_mapped = true
+			break
+			
+	if !action_mapped:
+		animation_player.play('rest')
+	
 func follow_mouse(event: InputEventMouseMotion):
 	var screen_position: Vector2 = event.position
 	var screen_position_normal: Vector2 = screen_position / get_viewport().get_visible_rect().size
